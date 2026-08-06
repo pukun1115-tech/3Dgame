519fcb1aecc348b217014b25a44ddcef924ab0eb
pukun1115-tech <pukun1115@gmail.com>
2026-08-06 14:23:02 +0900

    canvas描画用

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 function resize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
 function mainLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     //
+    requestAnimationFrame(mainLoop);
 }
 resize();
 mainLoop();
